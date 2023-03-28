294bc932f48429a740c7d5a505a4a55aec9beef0
Loodiie <120471071+Loodiie@users.noreply.github.com>
2023-03-28 08:59:34 +0200

    MAJ mentions légales

--- charte-mentions/mentions_legales.php
+++ charte-mentions/mentions_legales.php
@@ -18,28 +18,54 @@
     <body>
         <?php include ('../header_footer_body/header.php'); ?>
 
-        <main class="vertical"> 
+                <main class="vertical"> 
             <div class="title">
                 <h1>Mentions légales</h1>
             </div>
 
             <div class="text">
-                <p>Lorem ipsum dolor sit amet. Sit perspiciatis maxime sit error incidunt sed quod tenetur et molestiae natus est animi quia. 
-                    Sed laboriosam dolores qui vitae nobis in voluptas dolores sed tenetur animi aut ipsam iste. 
-                    Non illo quae ut voluptatibus sunt aut magnam consequatur qui distinctio reprehenderit est possimus atque non voluptas reiciendis in corrupti labore.
-
-                    <br>Est architecto magnam vel molestias pariatur et alias dolores nam voluptate dolores aut facere architecto a laboriosam saepe. 
-                    Eum obcaecati vero qui eaque provident aut pariatur vitae et cumque ipsam id pariatur vero.
+                <p>MENTIONS LEGALES :</br>
+                    Correspondant aux dispositions de l'article 6-III et 19 de la Loi numéro 2004-575 du 21 juin 2004 concerçant la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs du site internet : ENTRER L’URL Les infos suivantes :</br>
                     
-                    <br>Et harum labore cum aspernatur accusantium non impedit dolorem. Ea voluptas ipsa sit quasi similique aut sint corrupti ut autem autem id pariatur veniam et omnis dignissimos quo quia ipsum. 
-                    Et modi eius cum laboriosam modi hic quidem explicabo eum quisquam corrupti ut eaque blanditiis quo pariatur magnam. Vel accusantium omnis qui voluptas dicta sed vitae quasi At eius deserunt.
-                    <br>Lorem ipsum dolor sit amet. Ea nulla nisi id nemo sequi id voluptatum dolores sed suscipit repellat non incidunt aperiam ut laudantium repellat. 
-                    Sed quod quia in aperiam animi in doloremque perferendis qui beatae praesentium aut necessitatibus dignissimos. Nam voluptatem nihil ut expedita perspiciatis et magnam natus ut dicta voluptatum id culpa laborum.
-
-                    <br>Et provident officiis cum enim reprehenderit et sapiente itaque. Ea dolores possimus eum unde Quis ut dolorem rerum. 
-                    Rem autem molestiae qui velit voluptas et consequatur blanditiis in vitae praesentium sed omnis velit eos enim deserunt. Et tenetur asperiores ut eaque atque At rerum consequuntur et molestias quam.
+                </br>1. Déclaration légales :</br>
+                    </br> Propriétaire : Club de Robotique du CESI Brest</br>
+                    Adresse: 230 Rue Rolland Garros, 29490 Guipavas</br>
                     
-                    <br>Aut perferendis enim et nihil fugit qui magni quis quo perspiciatis vero hic nobis dolor. Qui quia odit ut similique accusantium qui ipsa dolor hic explicabo sapiente non nemo pariatur sit ipsam voluptas et nisi autem.
+                    </br>Fondatrices et réalisatrices du site internet : LE GOUYEN Elody / BOISSARD Justine / POOGEON Maewenn </br>
+
+                    </br>2. Présentation :</br>
+                    </br>Tout internaute se connectant et utilisant le site suivant : ENTRER L’URL est considéré comme utilisateur du site suivant.
+                    Le site internet ENTRER L’URL réunit un ensemble de services, dans l'état, disponible pour les usageés. Il est précisé que ces derniers doivent rester honnêtes et faire preuve de bonne foi tant envers les différents usagers qu'envers le Développeur du site ENTRER L’URL. Le site est mis à jour régulièrement par les Girl Power Robot.
+                    Les Girl Power Robot s'efforce de prescrire sur le site des informations les plus véridiques possibles (sous réserve de modifications apportées depuis leur mise en ligne), mais ne peuvent promettre l'exactitude, la complétude et l'actualité des informations transmises sur son site internet, qu'elles soient de son fait ou non. En dépendance, l'utilisateur reconnaît utiliser ces informations données (à titre indicatif, non exhaustives et susceptibles d'évoluer) sous sa responsabilité exclusive.</br>
+                    
+                    </br>3. Accessibilité Numérique :</br>
+                    
+                    </br>Le site internet est principalement accessible aux utilisateurs 24/24h, 7/7j, excepté en cas de maintenance, prévu ou non, pour les besoins de la mise à jour ou en cas de force majeure. En cas d'impossibilité d'accès au service, les Girl Power Robot s'engagent à faire leur maximum afin de rétablir l'accès au site et s'efforceront alors d'informer au préalable aux utilisateurs Dates & heures de l'interruption. N'étant soumis qu'à une redevance de moyen, le site ne saurait être désigné comme responsable des soucis, quelle qu'en soit la nature.</br>
+                    
+                    </br>4. Propriété intellectuelle :</br>
+                    
+                    </br>Les Girl Power Robot sont propriétaires exclusifs de tous les droits de propriété intellectuelle ou détient les droits d'usage sur tous les éléments accessibles sur le site internet, tant sur la structure que sur les articles et textes, images, graphismes, icônes, bande sonore, logiciels…
+                    Toute reproduction même partielle du site web, représentation, modification, adaptation totale ou partielle de l'un quelconque de ces éléments, sans tenir compte du moyen ou le procédé utilisé, est strictement interdite, excepté autorisation écrite préalable des Girl Power Robot, responsable du site internet même partielle du site internet, affichage, édition, adaptation totalement ou partiellement de l'un quelconque de ces éléments, peu importe le moyen ou le procédé utilisé, est strictement interdite, excepté acceptation préalable des Girl Power Robot, propriétaire du site web, à défaut elle sera désigné comme constitutive d'une contrefaçon et passible de poursuite d'après les dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</br>
+                    
+                    </br>5. Cookies et Liens Hypertextes :</br>
+                    
+                    </br>Le site contient un certain nombre de URL cliquables vers plusieurs sites web (partenaires, sites sources etc) mis en place suite à l'accord des  Girl Power Robot. Cependant,les Girl Power Robot ne peuvent pas vérifier l'intégralité du contenu des différents sites ainsi consultés et décline donc l'entière responsabilité de de cette façon quant aux risques de contenus illicites.
+                    L'usager est averti que durant ses visites sur le site web, un ou différents cookies peuvent s'installer de façon automatique sur son pc via son logiciel de navigation. Un cookie est un bloc de données qui ne permet pas de savoir qui est l'utilisateur, néanmoins qui note des indications en lien avec la navigation de l'utilisateur sur le site web. </br>
+                    
+                    
+                    </br>6. Protection des biens et des personnes - Maniement des données personnelles :</br>
+                    
+                    </br>En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978 , la loi n° 2004-801 datant du 6 août 2004, l'article L. 226-13 du Code pénal et la Directive Européenne datant du 24 octobre 1995.
+                    Sur le site web, les Girl Power Robot n'ont aucune information personnelle ( suivant l'article 4 loi n°78-17 du 06 janvier 1978) relatives à l'utilisateur que pour la nécessité de plusieurs services offerts par le site web. L'usager fournit les données en pleine conscience de cause, notamment quand il effectue lui-même la saisie.
+                    Conformément aux dispositions des articles 38 et autres de la loi 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux différentes libertés, n'importe quel utilisateur dispose d'un droit d'accès , de rectification, de suppression et d'opposition aux données personnelles le concernant. 
+                    Aucune information personnelle de l'utilisateur du site n'est publiée sans consentement de l'utilisateur, troquée, transmise, vendue sur n'importe quel support à des tiers. Uniquement l'hypothèse de la vente du site interne et de ses droits autorise aux Girl Power Robot à céder les informations à l'éventuel acquéreur qui serait lui-même tenu à la même nécessité de conservation et de modification des informations vis-à -vis de l'utilisateur du site interne.
+                    Le site internet est conforme avec le RGPD.</br>
+
+                    </br>7. Utilisation d’image libre de droit : </br>
+
+                    </br>Pour l’image de fond (robot qui lit) : 
+                    lien de téléchargement de l’image (qui ne semble plus disponible sur le site : https://wallpaperaccess.com/full/6870274.png )
+                    source : https://wallpaperaccess.com/ 
                 </p>
             </div>
         </main>
