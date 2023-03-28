5f40ae1ca95a79bb0dccad0262bf1c447a0bfbad
Maewenn <mw.porgeon@gmail.com>
2023-03-28 17:53:44 +0200

    comments contact.php & traitement.php

--- contact/contact.php
+++ contact/contact.php
@@ -3,89 +3,89 @@
 <html lang="fr">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!--Pour que la responsivité s'active quand on inspecte (F12)-->
-        <link rel="stylesheet" href="../header_footer_body/header_footer.css"> 
+        <!--Pour que la responsivité s'active quand on inspecte (F12)-->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+        <!-- lien pour le js -->
         <script src="../header_footer_body/header_anim.js" defer="defer"></script> 
         <script src="https://kit.fontawesome.com/d93ad3eb56.js" crossorigin="anonymous"></script>
-        
+        <!-- lien pour les fichiers de style (css) -->
         <link rel="stylesheet" href="contact.css"> 
         <link rel="stylesheet" href="../header_footer_body/body.css">
+        <link rel="stylesheet" href="../header_footer_body/header_footer.css"> 
+        <!--pour la typo google font Raleway-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400&display=swap" rel="stylesheet">  <!--pour la typo google font Raleway-->
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400&display=swap" rel="stylesheet">  
     </head>
 
     <body>
-        <?php include ('../header_footer_body/header.php'); ?>
+        <?php include ('../header_footer_body/header.php'); ?>   <!-- appel du fichier header.php-->
         
         <main>
-            <form action="traitement.php" method="post">
+            <!-- partie formulaire -->
+            <form action="traitement.php" method="post">     <!-- ouverture d'un formulaire pour après lier les données saisies par le user à la bdd -->
                 <div class="titre"> 
                     <h2>Laissez-nous un message</h2>
                 </div>
                 <div class="contenu">
-                    <div class="contact">
-                        <div class="contact_info">
+                    <div class="contact">               <!-- bloc de renseignement sur le user -->
+                        <div class="contact_info">      <!-- pour le champs où le user saisi son nom -->
                             <label for="name"></label>
                             <input type="text" id="name" name="user_name" 
                                 placeholder="Votre nom">
-                                <!--onfocus="if (this.value== this.defaultValue) this.value=''"
-                                onblur="if (this.value== '') this.value='Votre nom'"-->
                         </div>
 
-                        <div class="contact_info">
+                        <div class="contact_info">         <!-- pour le champs où le user saisi son email -->
                             <label for="email"></label>
                             <input type="email" id="email" name="user_email" 
                                 placeholder="Votre mail">
-                                <!--onfocus="if (this.value== this.defaultValue) this.value=''"
-                                onblur="if (this.value== '') this.value='Votre mail'"-->
                         </div>
                     </div>
 
-                    <div class="message">
-                        <div class="message_objet">
+                    <div class="message">                 <!-- bloc concernant le message -->
+                        <div class="message_objet">       <!-- pour le champs où le user saisi l'objet du message -->
                             <label for="objet"></label>
                             <input type="text" id="objet" name="user_objet" 
                                 placeholder="Objet du message">
-                               <!-- onfocus="if (this.value== this.defaultValue) this.value=''" 
-                                onblur="if (this.value== '') this.value='Objet du message'"-->
                         </div>
 
-                        <div class="message_contenu">  
+                        <div class="message_contenu">       <!-- pour le champs où le user saisi le message -->
                             <label for="message"></label>
                             <textarea type="text" id="message" name="user_message" placeholder="Tapez votre message ici"></textarea>
                         </div>
                     </div>
 
-                    <div class="bouton">
+                    <div class="bouton">                 <!-- le bouton qui permet d'envoyer les données saisies à la base de données contacts_site -->
                         <button type="submit">Envoyer</button>
                     </div>
                 </div>
             </form>
     
-            <section class="localisation">
+            <!-- partie CESI -->
+            <section class="localisation">            
                 <div class="titre"> 
                     <h2>Où nous trouver ?</h2>
                 </div>
                 
-                <div class="contenu">
+                <div class="contenu">   <!-- bloc pour le logo du CESI -->
                     <div class="coordo">
                         <div class="logo">
                             <img src="https://www.capitainestudy.fr/wp-content/uploads/2019/02/cesi_RVB.png">
                         </div>
-                        <div class="adresse"> 
+                        <div class="adresse">     <!-- bloc pour l'adresse du CESI -->
                             <p>CESI</p>
                             <p>230 rue Rolland Garros</p>
                             <p>29480 Guipavas</p>
                             <a href="https://brest.cesi.fr/">CESI Brest</a>
                         </div>
                     </div>
+                    <!-- Map pour trouver le CESI Brest -->
                     <iframe src="https://www.google.com/maps/d/embed?mid=1K26ZFy1FHb_nfA7m-ERMWL-t4cxed-g&ehbc=2E312F"></iframe> <!-- width="640" height="480"-->
                 </div>
             </section>
         </main>
         
-        <?php include('../header_footer_body/footer.php'); ?>
+        <?php include('../header_footer_body/footer.php'); ?> <!-- appel du fichier footer.php-->
         
     </body>
 </html>
--- contact/traitement.php
+++ contact/traitement.php
@@ -1,47 +1,41 @@
 <?php 
 
-/*if (!isset($_POST)) {
-    return;
-}*/
-
 try {
-    $db = New PDO('mysql:host=localhost;dbname=contacts_site;charset=utf8','root', '');
+    $db = New PDO('mysql:host=localhost;dbname=contacts_site;charset=utf8','root', ''); /*connexion à la bdd créée pour la récupération des données */
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();           /* pour avoir un message d'erreur si la connexion n'aboutie pas*/
     return;
 }
 
 if(!empty($_POST["user_name"]) && !empty($_POST["user_email"]) 
-    && !empty($_POST["user_objet"]) && !empty($_POST["user_message"])){
+    && !empty($_POST["user_objet"]) && !empty($_POST["user_message"])){   /*on vérifie s'il y a des données saisies dans les champs du formulaire */
 
-        $user_name = htmlspecialchars($_POST["user_name"]);
+        $user_name = htmlspecialchars($_POST["user_name"]);     /*récupération des données saisies dans le champs user_name en prenant en compte les caractères spéciaux*/
         $user_email = htmlspecialchars($_POST["user_email"]);
         $user_objet = htmlspecialchars($_POST["user_objet"]);
         $user_message = htmlspecialchars($_POST["user_message"]);
 
-        $s="INSERT INTO contact (user_name,user_email,user_objet,user_message) 
+        $s="INSERT INTO contact (user_name,user_email,user_objet,user_message) /* requète sql qui insérera/créera les données saisies dans le formulaire contact -> dans la bdd*/
                 VALUES (:user_name,:user_email,:user_objet,:user_message)";
         
-        $r = $db->prepare($s);
+        $r = $db->prepare($s);     /* on prépare la requête */
         
-        $r->BindValue(':user_name',$user_name);
+        $r->BindValue(':user_name',$user_name);     /*Associe la valeur saisie dans le champs du nom au paramètre user_name*/
         $r->BindValue(':user_email',$user_email);
         $r->BindValue(':user_objet',$user_objet);
         $r->BindValue(':user_message',$user_message);
        
-        $ok = $r->execute();
+        $ok = $r->execute();    /*exécute la requête préparée */
         
-        if(!$ok){
-            die ("Une erreur est survenue");
+        if(!$ok){                             /*si la requête ne s'exécute pas correctement */
+            die ("Une erreur est survenue");   /*la procédure s'arrète et un message d'erreur s'affiche */
         }else{
-            echo "Votre message a bien été transmis. Nous vous remercions de nous avoir contacté
-            et nous vous répondrons dès que possible. A bientôt ! Le club de robotique CESI de Brest" ;
+            echo "Votre message a bien été transmis. Nous vous remercions de nous avoir contacté      
+            et nous vous répondrons dès que possible. A bientôt ! Le club de robotique CESI de Brest" ; /*si les données remontent bien en bdd, un message de remerciement s'affiche*/
         
         }
 }else{
-    die("Le formulaire est incomplet. Veuillez remplir tous les champs, s'il vous plait. Merci !");
+    die("Le formulaire est incomplet. Veuillez remplir tous les champs, s'il vous plait. Merci !");  /*si tous les champs ne sont pas remplis et que le user submit, un message d'erreur s'affiche */
 }
-
-
 
 ?>
